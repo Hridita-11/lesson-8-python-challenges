c1c523c43e471457d008fa05c077d7f15691678d
Hridita-11 <hridita.nz@gmail.com>
2026-02-17 19:18:07 +1300

    Add files via upload

---
+++ operator_precedence.py
@@ -0,0 +1,14 @@
+a = 4 
+b = 7
+c= 9
+d= 2
+e= 8
+e= (a+b) * a/d;
+print("Value of (a+b) a/d ", e)
+name  ="alex"
+age = 55
+if name== "alex" or name== "Marco" and age >=78:
+    print(" Hello! we;come")
+else:
+    print("Goodbye!")
+
